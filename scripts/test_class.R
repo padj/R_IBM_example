16923c9ecf4fdfad579463dd774ef739e63a303a
Thomas Padgett <thomas.padgett@heor.co.uk>
2022-12-09 15:29:55 +0000

    Create test_class.R

---
+++ scripts/test_class.R
@@ -0,0 +1,44 @@
+
+## Define the class
+test_class <- setRefClass(
+  "test_class",
+  fields = list(
+    x = "ANY",
+    y = "numeric",
+    z = "character"
+  ),
+  methods = list(
+    
+    initialize = function(x = NULL, 
+                          y = 1:10, 
+                          z = letters){
+      "This method is called when you create an instance of the class."
+      x <<- x
+      y <<- y
+      z <<- z
+      print("You initialized MyClass!")
+    },
+    
+    hello = function(){
+      "This method returns the string 'hello'."
+      "hello"
+    },
+    
+    doubleY = function(){
+      y <<- 2 * y
+    }
+    ,
+    printInput = function(input){
+      if(missing(input)) stop("You must provide some input.")
+      print(input)
+    }
+  )
+)
+
+
+## Create the class
+obj1 <- test_class$new()
+
+obj1$y
+obj1$doubleY()
+obj1$y
